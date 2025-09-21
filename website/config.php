8d27088aaa535c2434227ebbc3c1309c61ecb57f
mala <mala@Sureshs-MacBook-Pro.local>
2025-09-21 18:29:27 -0400

    Added my initial to config.php

--- website/config.php
+++ website/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website',
+        'name' => 'Simple PHP Website - Deployed Mala',
         'nav_menu' => [
             '' => 'Home',
             'content/about-us.php' => 'About Us',
